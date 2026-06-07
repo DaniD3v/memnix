4fd6c3d4c9c4d9e5e5b703c264cbcf96c1f58a2d
DaniD3v <sch220233@spengergasse.at>
2026-06-07 02:23:33 +0200

    chore: remove unused mir error

--- src/mir/error.rs
+++ src/mir/error.rs
@@ -4,9 +4,6 @@ use crate::mir::Ident;
 
 #[derive(Error, Debug)]
 pub enum MirResolveError {
-    #[error("attempted calling something that's not a lambda")]
-    NotALambda,
-
     #[error("the identifier {} could not be resolved", .0.as_ref())]
     IdentUnresolvable(Ident),
 }
