7548afbe33668dd5435a51c73e0a99b9a78a570e
DaniD3v <sch220233@spengergasse.at>
2026-06-09 18:02:46 +0200

    feat: impl Resolver for &impl Resolver

--- src/mir/ident_resolver/traits.rs
+++ src/mir/ident_resolver/traits.rs
@@ -2,6 +2,16 @@ use bumpalo::Bump;
 
 use crate::mir::{Expr, Ident, error::MirResolveError, lang::Intrinsics};
 
+/// Ast type that can be resolved to a Mir type
+pub trait Resolve: Sized {
+    type Target<'a>;
+    fn resolve<'bump>(
+        self,
+        resolver: &impl Resolver<'bump>,
+        bump: &'bump Bump,
+    ) -> Result<Self::Target<'bump>, MirResolveError>;
+}
+
 pub trait Resolver<'bump> {
     fn resolve_ident(
         &self,
@@ -14,12 +24,18 @@ pub trait Resolver<'bump> {
     fn get_intrinsics(&self) -> &'bump Intrinsics<'bump>;
 }
 
-/// Ast type that can be resolved to a Mir type
-pub trait Resolve: Sized {
-    type Target<'a>;
-    fn resolve<'bump>(
-        self,
-        resolver: &impl Resolver<'bump>,
-        bump: &'bump Bump,
-    ) -> Result<Self::Target<'bump>, MirResolveError>;
+impl<'b, T: Resolver<'b>> Resolver<'b> for &T {
+    fn resolve_ident(
+        &self,
+        ident: &Ident,
+        bump: &'b Bump,
+    ) -> Result<&'b Expr<'b>, MirResolveError> {
+        (*self).resolve_ident(ident, bump)
+    }
+    fn get_param_nesting_depth(&self) -> usize {
+        (*self).get_param_nesting_depth()
+    }
+    fn get_intrinsics(&self) -> &'b Intrinsics<'b> {
+        (*self).get_intrinsics()
+    }
 }
